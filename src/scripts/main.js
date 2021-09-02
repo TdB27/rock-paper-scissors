5cde1ef6b8baf38b7c982ce8ee13638c4c63cdfc
Thiago Billy <thiagobilly27@gmail.com>
2021-09-02 16:00:44 -0300

    inserir a borda e o card para o player e computer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-// iniciar jogo
+// start game
 playerScore = 0
 
 function game() {
@@ -7,24 +7,67 @@ function game() {
   selectCard.forEach(item => {
     item.addEventListener('click', () => {
       const itemCard = item.getAttribute('data-card').toLowerCase()
-      
+
       let playerSelect = itemCard
       let computerSelect = computerPlay()
 
-      playRound(playerSelect, computerSelect)
+      const hiddenTriangle = document.querySelector('.cards-game__bg-triangle')
+      const showRound = document.querySelector('.cards-game__round')
+      const showWinner = document.querySelector('.cards-game__round__box-winner')
 
+      hiddenTriangle.classList.add('hidden-game')
+      showRound.classList.remove('hidden-result')
+      showWinner.classList.add('active')
+
+      playRound(playerSelect, computerSelect)
+      card(playerSelect, computerSelect)
+
+      console.log("player " + playerSelect)
+      console.log("computer " + computerSelect)
+      
       let score = document.querySelector('.score')
       score.innerHTML = playerScore
     })
   })
 }
 
-// computer escolhe aleatoriamente
+// insert the edge and the card to player and computer
+function card(player, computer){
+  const borderPlayer = document.querySelector('.cards-game__round__game__image.player')
+  const cardPlayer = document.querySelector('.cards-game__round__game__image-bg.player')
+  if(player === 'paper'){
+    borderPlayer.classList.add('picked-paper-border')
+    cardPlayer.classList.add('picked-paper-card')
+  } else if(player === 'scissors'){
+    borderPlayer.classList.add('picked-scissors-border')
+    cardPlayer.classList.add('picked-scissors-card')
+  } else if(player === 'rock'){
+    borderPlayer.classList.add('picked-rock-border')
+    cardPlayer.classList.add('picked-rock-card')
+  }
+  
+  const borderComputer = document.querySelector('.cards-game__round__game__image.computer')
+  const cardComputer = document.querySelector('.cards-game__round__game__image-bg.computer')
+  if(computer === 'paper'){
+    borderComputer.classList.add('picked-paper-border')
+    cardComputer.classList.add('picked-paper-card')
+  } else if(computer === 'scissors'){
+    borderComputer.classList.add('picked-scissors-border')
+    cardComputer.classList.add('picked-scissors-card')
+  } else if(computer === 'rock'){
+    borderComputer.classList.add('picked-rock-border')
+    cardComputer.classList.add('picked-rock-card')
+  }
+
+}
+
+// computer random pick
 function computerPlay() {
   let random = ['rock', 'paper', 'scissors']
   return random[Math.floor(Math.random() * 3)].toLowerCase()
 }
 
+//
 function playRound(playerSelect, computerSelect) {
   if(playerSelect === computerSelect) {
     return console.log("Empate")
@@ -33,7 +76,6 @@ function playRound(playerSelect, computerSelect) {
       return console.log("computer won")
     } else {
       playerScore++
-      console.log("o score do player é " + playerScore)
       return console.log("player won")
     }
   } else if (playerSelect === 'scissors'){
@@ -41,7 +83,6 @@ function playRound(playerSelect, computerSelect) {
       return console.log("computer won")
     } else {
       playerScore++
-      console.log("o score do player é " + playerScore)
       return console.log("player won")
     }
   } else if (playerSelect === 'rock'){
@@ -49,10 +90,11 @@ function playRound(playerSelect, computerSelect) {
       return console.log("computer won")
     } else {
       playerScore++
-      console.log("o score do player é " + playerScore)
       return console.log("player won")
     }
   }
 }
 
 game()
+
+//quando clicar no player add 
