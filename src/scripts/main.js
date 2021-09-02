0867a8aec6b6469e12393f7191a6cfe6b37436c5
Thiago Billy <thiagobilly27@gmail.com>
2021-09-02 15:11:22 -0300

    inserindo a pontuação de cada partida

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,22 @@
 // iniciar jogo
+playerScore = 0
+computerScore = 0
+match = 0
+
 function game() {
   let selectCard = document.querySelectorAll('[data-card]')
 
   selectCard.forEach(item => {
     item.addEventListener('click', () => {
       const itemCard = item.getAttribute('data-card').toLowerCase()
-
+      
       let playerSelect = itemCard
       let computerSelect = computerPlay()
+      
+      /*console.log("o score do player é " + playerScore)
+      console.log("o score do computer é " + computerScore) */
+      match++
+      console.log(match)
 
       playRound(playerSelect, computerSelect)
 
@@ -26,20 +35,32 @@ function playRound(playerSelect, computerSelect) {
     return console.log("Empate")
   } else if (playerSelect === 'paper'){
     if (computerSelect === 'scissors') {
+      computerScore++
+      console.log("o score do computer é " + computerScore)
       return console.log("computer won")
     } else {
+      playerScore++
+      console.log("o score do player é " + playerScore)
       return console.log("player won")
     }
   } else if (playerSelect === 'scissors'){
     if (computerSelect === 'rock') {
+      computerScore++
+      console.log("o score do computer é " + computerScore)
       return console.log("computer won")
     } else {
+      playerScore++
+      console.log("o score do player é " + playerScore)
       return console.log("player won")
     }
   } else if (playerSelect === 'rock'){
     if (computerSelect === 'paper') {
+      computerScore++
+      console.log("o score do computer é " + computerScore)
       return console.log("computer won")
     } else {
+      playerScore++
+      console.log("o score do player é " + playerScore)
       return console.log("player won")
     }
   }
